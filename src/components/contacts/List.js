7d15a3791776ea73693e2ffb36cfe4208f7eb6fd
ShmsErl <erol.sehmus1@gmail.com>
2023-12-24 19:42:11 +0300

    Eklenen kişilerin listeleneceği list şablonu tasarlandı.

--- src/components/contacts/List.js
+++ src/components/contacts/List.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({ contacts }) {
+
+    const [filterText,setFilterText] = useState("") 
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key)=> {
+         return   item[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase())
+        } );
+    });
+
+    console.log(filtered)
     return (
         <div>
-            <ul >
+             
+
+            <input type='text' placeholder='Filter Contact' value={filterText} onChange={(e)=>setFilterText(e.target.value)} />
+            <ul className='list' >
                 {
 
-                    contacts.map((value, index) =>
+                    filtered.map((value, index) =>
 
                         <li key={index} >
-                            {index}
-                            {value.name}
+                            
+                            {`${value.name} ${value.surname}`}
 
                         </li>
 
@@ -18,7 +30,9 @@ function List({ contacts }) {
 
 
                     )
-                } </ul>
+                }  {filtered.length}
+                </ul>
+               
         </div>
     )
 }
